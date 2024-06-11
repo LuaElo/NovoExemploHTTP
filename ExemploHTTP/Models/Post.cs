5252efe0cbf8e0e841cdb407b9837ef046b2a05b
eloahluara <eloahluara123@gmail.com>
2024-06-11 12:36:29 -0300

    Exemplo HTTP

---
+++ ExemploHTTP/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploHTTP.Models
+{
+    public class Post
+    { 
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        private string Title { get; set; }
+        private string Body { get; set; }
+
+    }
+}
